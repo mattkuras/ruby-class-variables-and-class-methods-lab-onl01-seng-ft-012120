866db76e1d978051545c418e27621016a7ee867d
Matt Kuras <mattkuras@gmail.com>
2020-02-01 22:00:56 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song 
+
+@@song_count = 0 
+
+def self.count 
+  @@song_count
+end 
+
+@@genres = []
+
+def self.genres
+  @@genres
+end 
+
+def self.genre_count
+
+@@artists = []
+
+def self.artists 
+  @@artists
+end 
+
+attr_accessor :name, :artist, :genre
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist 
+  @genre = genre 
+  @@song_count + 1 
+  if @@genres!.include?(genre)
+    @@genres << genre 
+  @@artists + 1 
+end 
+
+def self.count 
+  @@song_count
+end 
+
+
+
+
+  
+
+end
