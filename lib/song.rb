2877e0042d17f9e5e4af0ecc7b39e899153e4fc0
Matt Kuras <mattkuras@gmail.com>
2020-02-01 22:48:48 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,56 @@
+class Song 
+  
+@@count = 0
+
+def self.count
+  @@count 
+end 
+
+@@genres = []
+
+def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+  
+def self.genres
+  @@genres.uniq
+end
+
+@@artists = []
+
+def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
+def self.artists
+    @@artists.uniq
+end
+
+attr_accessor :name, :artist, :genre 
+
+def initialize(name, artist, genre)
+   @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end 
+  
+end
