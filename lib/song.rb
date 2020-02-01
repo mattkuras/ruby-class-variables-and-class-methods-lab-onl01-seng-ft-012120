1b25a00da30afa9df3cbd041f220e4e32dc7bdf6
Matt Kuras <mattkuras@gmail.com>
2020-02-01 22:32:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,19 @@
 class Song 
   
-@@count = 0   
-  
+@@count = 0
+
+def self.count
+  @@count 
+end 
+
+@@genres = []
+
+
+
 attr_accessor :name, :artist, :genre 
 
 def initialize(name, artist, genre)
-  @@count 
+  @@count += 1 
+  @@genres += 1 
   
 end
