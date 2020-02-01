cdf810ab67393f63ef4534be5b59dd1ec7e0fc8e
Matt Kuras <mattkuras@gmail.com>
2020-02-01 22:46:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,15 +23,7 @@ def self.genre_count
 @@artists = []
 
 def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1 
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
+    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
   end
 
 
