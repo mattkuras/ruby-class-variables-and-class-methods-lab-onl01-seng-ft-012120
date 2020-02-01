dbeb0afac73c21968b36cec91c149419a173e6c5
Matt Kuras <mattkuras@gmail.com>
2020-02-01 22:42:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,6 +22,18 @@ def self.genre_count
 
 @@artists = []
 
+def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    genre_count
+  end
+
 
 attr_accessor :name, :artist, :genre 
 
